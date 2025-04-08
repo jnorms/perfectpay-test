43d6f3cdf2ce94243016dd75e1ed381e3a9d610f
José Normando <jnorms2699@icloud.com>
2025-04-08 18:20:49 -0300

    [feature/asaas-customer-integration]: feat - adicionando integração com asaas para cadastro de clientes

---
+++ app/Domain/Entities/Asaas/AsaasEntityInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Domain\Entities\Asaas;
+
+interface AsaasEntityInterface
+{
+    public function getAsaasBody(): array;
+}
--- tests/Feature/Integrations/AsaasTest.php
+++ tests/Feature/Integrations/AsaasTest.php
@@ -4,7 +4,7 @@ namespace Tests\Feature\Integrations;
 
 use App\Domain\Entities\Asaas\Customer\Customer;
 use App\Domain\Entities\Asaas\Customer\CustomerAddress;
-use App\Domain\Integrations\Asaas\CreateCustomerAsaasIntegration;
+use App\Domain\Integrations\Asaas\Customer\CreateCustomerAsaasIntegration;
 use Illuminate\Http\Client\RequestException;
 use Tests\TestCase;
 
@@ -37,7 +37,7 @@ class AsaasTest extends TestCase
     }
     
     /**
-     * Validate that the asaas integration has bad request status code a send request without token
+     * Validate that the asaas integration has bad request status code a send request without user agent
      */
     public function test_validate_assas_http_macro_without_user_agent_header(): void
     {
@@ -49,5 +49,41 @@ class AsaasTest extends TestCase
         } catch (\Throwable $exception) {
             $this->fail($exception->getMessage());
         }
+    }
+    
+    public function test_create_an_asaas_customer(): void
+    {
+        $customerAddress = $this->app->make(CustomerAddress::class);
+        $customerAddress->create(
+            address: $this->faker->streetAddress,
+            number:   $this->faker->buildingNumber,
+            complement: null,
+            neighborhood: $this->faker->words(rand(1, 3), true),
+            postcode: $this->faker->postcode,
+        );
+        $customer = $this->app->make(Customer::class);
+        $customer->create(
+            name    : $this->faker->name,
+            document: $this->faker->randomElement(
+                          [
+                              $this->faker->cpf(false),
+                              $this->faker->cnpj(false),
+                          ]
+                      ),
+            email: $this->faker->email,
+            mobilePhone: $this->faker->phoneNumberCleared(false),
+            address: $customerAddress
+        );
+        $integration = $this->app->make(CreateCustomerAsaasIntegration::class);
+        $integration->setCustomer($customer);
+        $integration->handle();
+        $response = $integration->getResponse();
+        $this->assertTrue($response->successful());
+        $body = $integration->getResponseBody();
+        $this->assertArrayHasKey('id', $body);
+        $this->assertEquals($customer->getName(), $body['name']);
+        $this->assertEquals($customer->getDocument(), $body['cpfCnpj']);
+        $this->assertEquals($customer->getEmail(), $body['email']);
+        $this->assertEquals($customer->getMobilePhone(), $body['mobilePhone']);
     }
 }
